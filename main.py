2d4ac8b60962af1284311667303793a58a1aa021
beavanzi <bia.avanzi@gmail.com>
2021-10-27 00:34:31 -0300

    feat: add parser and auxiliary fns

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 class Node:
 
     def __init__(self, value: object):
@@ -21,8 +17,19 @@ class Node:
     def getRightChild(self):
         return self.rightChild
 
-    def getItself(self):
+    def getValue(self):
         return self.value
+
+
+class Tree:
+    
+    def __init__(self):
+        root = None
+        height = None
+
+    def toString(self):
+        return 0
+
 
 class Stack:
 
@@ -41,6 +48,10 @@ class Stack:
     def size(self):
         return len(self.items)
 
+    def getItems(self):
+        return self.items
+
+
 class Queue:
 
     def __init__(self):
@@ -52,39 +63,109 @@ class Queue:
     def dequeue(self):
         return self.items.pop(0)
 
-    def peek(self):
-        return self.items[-1]
+    def peekFirst(self):
+        return self.items[0]
 
     def size(self):
         return len(self.items)
+
+    def getItems(self):
+        return self.items
+
+    def reverseQueue(self):
+        self.items.reverse()
 
 
 def isOperator(op):
     return op == "+" or op == "-" or op == "*" or op == "/"
 
 
-def toTree(arr, elem: None or int):
+def toTree(arr: list, elem: str or Node or int):
     if elem is not Node:
         node = Node(elem)
     else:
         node = elem
 
-    if isOperator(node.value):
-        if node.rightChild is None:
+    if isOperator(node.getValue()):
+        if node.getRightChild() is None:
             node.setRightChild(arr.pop(0))
-            toTree(arr, node.rightChild)
-        if node.leftChild is None:
+            toTree(arr, node.getRightChild())
+        if node.getLeftChild() is None:
             node.setLeftChild(arr.pop(0))
-            toTree(arr, node.leftChild)
-    print(node.value)
+            toTree(arr, node.getLeftChild())
+    print(node.getValue())
 
 
+def popAndEnqueueInternalParentesisContent(stack, queue):
+    while stack.peek() != "(":
+        popAndEnqueue(stack, queue)
+    stack.pop()
 
 
-# Press the green button in the gutter to run the script.
+def popAndEnqueue(stack, queue):
+    item = stack.pop()
+    if item != "(" or item != ")":
+        queue.enqueue(item)
+
+
+def evalStep():
+    return 0
+
+
+def parser(tokens: list):
+    stack = Stack()
+    queue = Queue()
+
+    priorityOne = ["+", "-"]
+    priorityZero = ["*", "/"]
+
+    for token in tokens:
+        if isOperator(token):
+            if stack.size() > 0 and stack.peek() in priorityZero:
+                if token in priorityZero:
+                    stack.push(token)
+                elif token in priorityOne:
+                    popAndEnqueue(stack, queue)
+                    stack.push(token)
+            else:
+                stack.push(token)
+        elif token == "(":
+            stack.push(token)
+        elif token == ")":
+            popAndEnqueueInternalParentesisContent(stack, queue)
+        else:
+            queue.enqueue(token)
+
+    # Se restar algo na stack (exemplo: parentenses extras) entao a operaçao é inválida e nao será realizada
+    if stack.size() != 0:
+        print("Operação Inválida!")
+        exit(0)
+
+    # Reverter a queue pra que a ordem de avaliação das expressões fique correta
+    queue.reverseQueue()
+
+    root = queue.dequeue()
+
+    # Transformando a queue em lista simples para que seja mais fácil de lidar
+    listOfNodes = queue.getItems()
+
+    toTree(listOfNodes, root)
+
+
+def lexer(op: str):
+    tokens = op.split(" ")
+
+    # Adiçao de parenteses na expressão geral para que a parse funcione corretamente
+    tokens.insert(0, "(")
+    tokens.insert(-1, ")")
+
+    return tokens
+
+
 if __name__ == '__main__':
-    arr = ["+", "/", "-", 3, 9, 18, 4]
-    root = arr.pop(0)
-    toTree(arr, root)
+    # arr = ["+", "/", "-", 3, 9, 18, 4]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    operation = "4 + 18 / ( 9 - 3 )"
+    tokens = lexer(operation)
+    parser(tokens)
+
